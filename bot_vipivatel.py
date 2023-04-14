b1a4240a6d2005175c0faf69414912a29f0a7f24
pats6 <112085269+pats6@users.noreply.github.com>
2023-04-14 12:14:00 +0300

    апдейтик ботана
    добавлена нумерация коктейлей, реджекты плохих рецептов

--- bot_vipivatel.py
+++ bot_vipivatel.py
@@ -3,12 +3,19 @@ from numpy.random import randint
 from nekot import TOKEN
 
 START_MSG = "привет, коллега, напиши что у тебя есть, что ты хочешь, формат можешь подглядеть"
-FORMAT_MSG = "Ингредиенты:\nвино с водой\nсок\nсидр\nКачества:\nсладко\nне крепко\nкисло"
-END_MSG = "интересный вкус, а вот и рецепт"
+FORMAT_MSG = "Ингредиенты:\nвино с водой\nсок\nсидр\nКачества:\nсладко\nне крепко\nкисло\n\nкоктейль не понравился? удали рецепт сообщением \"*номер*!\""
+END_MSG1 = "интересный вкус, а вот и рецепт"
+END_MSG2 = "оки доки, босс, вот твой коктейль"
+END_MSG3 = "коктейли, не можешь с ними, не можешь без них"
+NUM_REC = "номер твоего коктейля "
+NUM_REMOVE = "удалил, чекай братик"
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot=bot)
 dict_of_cockts = {}
+num_dict = {}
+cock_num = 0
+wrong_nums = []
 
 
 @dp.message_handler(commands=['start'])
@@ -23,9 +30,21 @@ async def format_handler(message: types.Message):
 
 @dp.message_handler()
 async def send_handler(message: types.Message):
+    global cock_num
     text = message.text
-    if text.count(':') != 2:
+
+    if text.isdigit() and int(text) <= cock_num and wrong_nums.count(int(text)) == 0:
+        await message.answer(dict_of_cockts[num_dict[int(text)]])
+        await message.answer(END_MSG3)
+
+    elif text[:-1].isdigit() and text[-1:] == "!" and int(text[:-1]) <= cock_num:
+        dict_of_cockts.pop(num_dict[int(text[:-1])])
+        wrong_nums.append(int(text[:-1]))
+        await message.answer(NUM_REMOVE)
+
+    elif text.count(':') != 2:
         await message.answer("wrong door")
+
     else:
         inp = str(text).split(':')
         ings = inp[1].split('\n')
@@ -33,11 +52,13 @@ async def send_handler(message: types.Message):
         ans = ""
         prop = []
         sum_int = 0
+
         if tuple(ings) in dict_of_cockts:
             ans = dict_of_cockts[tuple(ings)]
+
         else:
             for i in range(len(ings)-2):
-                help_int = randint(1, 10)
+                help_int = randint(1, 6)
                 sum_int += help_int
                 prop.append(help_int)
             for i in range(1, len(ings)-1):
@@ -46,9 +67,23 @@ async def send_handler(message: types.Message):
                 else:
                     added = str(prop[i-1])+"/"+str(sum_int)
                 ans += str(ings[i])+" : "+added+"\n"
-                dict_of_cockts[tuple(ings)] = ans
-        await message.answer(END_MSG)
+            dict_of_cockts[tuple(ings)] = ans
+            cock_num += 1
+            num_dict[cock_num] = tuple(ings)
+
+        msg_num = randint(1, 4)
+
+        if msg_num == 1:
+            await message.answer(END_MSG1)
+
+        elif msg_num == 2:
+            await message.answer(END_MSG2)
+
+        else:
+            await message.answer(END_MSG3)
+
         await message.answer(ans)
+        await message.answer(NUM_REC + str(cock_num))
 
 
 if __name__ == "__main__":
